dfbf9f93ca9846f3d099a021259c5283225d1d53
shubhaangi620 <subhaangik@gmail.com>
2024-01-05 10:01:58 +0530

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 4566
+let accountEmail = "shu@gmail.com";
+var accountPassword = " shim432";
+accountCity = "jaipur"
+
+// accountId=23  //not allowed
+accountEmail = "pik@fmail"
+accountPassword = "867yi7"
+accountCity = "mumbai"
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+// prefer not to use var
+// because of issue in block and functional scope
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+
+//  PRIMITIVE DATATYPES
+// numbers => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => when value is not defined
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
+
+
